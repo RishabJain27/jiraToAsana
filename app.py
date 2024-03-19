b5590b164f01acd3bf23a6a9db54aec01fc97797
Rishab Jain <rishab@Rishabs-MacBook-Air.local>
2024-03-19 14:28:35 -0700

    restart

--- app.py
+++ app.py
@@ -3,26 +3,19 @@ from flask import request
 import requests
 from requests.auth import HTTPBasicAuth
 from pprint import pprint
+import json
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def start():
+@app.route('/jiraWebHook', methods=['GET','POST'])
+def webHook():
+    pprint('HERE')
     return 'Hello World'
 
-@app.route('/jiraWebHook', methods=['GET', 'POST'])
-def receivedWebHook():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        req_data = request.get_json()
-        return req_data
-    return ''
 
 @app.route('/jiraProjects', methods=['GET'])
 def getProjects():
     url = "https://442561275-team-mcpys4njze6e.atlassian.net/rest/api/3/project/search"
-    temp = "trash"
 
     auth = HTTPBasicAuth("alan-phnx@442561275.asanatest1.us", "ATATT3xFfGF0dEdMkTXmZ4gXrwLmIYq87EqI_BSapeR_NcLmHZGZ6-HU86lD0RmRgUzbkFAP0Ardw244vvVqG5XtPaLn_yA8ebOOWrbIUPj6A38hoylAYctjubKILRveFkwL4V1HNDLjJuBJVGAG4zuIKNm8vOBFoFOlDJ5reAEWhO4Xs47-3-M=BA3485FD")
 
@@ -37,7 +30,7 @@ def getProjects():
     auth=auth
     )
 
-    return response.text
+    return response
 
 
 if __name__ == "__main__":
